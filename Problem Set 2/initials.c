6bc83e6e1cd48c9164bb55d42a5dd08b20668e6c
Chris Koro <chriskoro92@gmail.com>
2016-12-27 02:01:08 -0600

    Update initials.c

--- Problem Set 2/initials.c
+++ Problem Set 2/initials.c
@@ -1,3 +1,6 @@
+/*Program takes the provided user's name,
+and displays the capitalized initials*/
+
 #include <stdio.h>
 #include <cs50.h>
 #include <ctype.h>
@@ -5,9 +8,7 @@
 
 int main(void)
 {
-
     string name = GetString();
-    
     
     for(int i = 0, n = strlen(name); i < n; i++)
     {
@@ -21,8 +22,6 @@ int main(void)
             printf("%c", toupper(name[i+1]));
             i++;
         }
-        
-        
     }
     
     printf("\n");
